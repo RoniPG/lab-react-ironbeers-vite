d9e9c59b77de9a69980b09be9a1a4e362a47550e
RoniPG <ronigmn@gmail.com>
2024-12-06 05:46:25 +0100

    5.2 | Make a request to the API

--- src/pages/BeerDetailsPage.jsx
+++ src/pages/BeerDetailsPage.jsx
@@ -1,8 +1,19 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 
 function BeerDetailsPage() {
+    const [beer, setBeer] = useState();
     const { beerId } = useParams();
-    
+    const getBeer = () => {
+        axios
+        .get(`https://ih-beers-api2.herokuapp.com/beers/${beerId}`)
+        .then(response => setBeer(response.data))
+        .catch(err => console.error(err));
+    }
+    useEffect(() => {
+        getBeer()
+    }, [beerId]);
     return (
         <>
         </>

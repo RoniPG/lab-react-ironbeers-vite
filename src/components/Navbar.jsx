19ab0244aa44bd6731e92b3a78785830d0826145
RoniPG <ronigmn@gmail.com>
2024-12-06 04:22:33 +0100

    Iteration 3 | Navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,3 +1,16 @@
-function Navbar() {}
+import { Link } from "react-router-dom";
+import HomeIcon from "../assets/home-icon.png"
+
+function Navbar() {
+    return (
+        <>
+            <nav className="d-flex py-3 my-1 bg-primary justify-content-center">
+                <Link to={"/"}>
+                    <img src={HomeIcon} alt="Home Icon" />
+                </Link>
+            </nav>
+        </>
+    );
+}
 
 export default Navbar;
